803ec38d0ff8c650e353ff206a84e76c6f8e27c5
Tanguy Kervran <tanguy.kervran@external.desouttertools.com>
2023-01-11 09:25:30 +0100

    🚀 Engram layout added

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -4,10 +4,9 @@
 
 #define _ISRT 0
 #define _DVORAK 1
-#define _FN 2
-#define _LOWER 3
-#define _RAISE 4
-#define _ADJUST 5
+#define _ENGRAM 2
+#define _FN 3
+#define _LOWER 4
 
 #define SHIFT_NO(kc1, kc2) \
 if (record->event.pressed) { \
@@ -29,10 +28,9 @@ if (record->event.pressed) { \
 enum custom_keycodes {
   ISRT = SAFE_RANGE,
   DVORAK,
+  ENGRAM,
   FN,
   LOWER,
-  RAISE,
-  ADJUST,
   ONE,
   TWO,
   THREE,
@@ -43,6 +41,25 @@ enum custom_keycodes {
   EIGHT,
   NINE,
   ZERO,
+
+  E_ONE,
+  E_TWO,
+  E_THREE,
+  E_FOUR,
+  E_FIVE,
+  E_SIX,
+  E_SEVEN,
+  E_EIGHT,
+  E_NINE,
+  E_ZERO,
+
+  E_QUOT,
+  E_DQUO,
+  E_COMM,
+  E_DOT,
+  E_MINS,
+  E_QUES,
+
   QUOTES,
   COLUMN,
   SLASH,
@@ -64,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, FR_I,    FR_S,    FR_R,    FR_T,    FR_G,                               FR_P,    FR_N,    FR_E,    FR_A,    FR_O,    KC_ENT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, FR_Q,    FR_V,    FR_W,    FR_D,    FR_J,    KC_LGUI,          KC_END,  FR_B,    FR_H,    SLASH,   DOT,     FR_X,    DVORAK,
+     KC_LCTL, FR_Q,    FR_V,    FR_W,    FR_D,    FR_J,    KC_LGUI,          KC_END,  FR_B,    FR_H,    SLASH,   DOT,     FR_X,    _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LALT, KC_LCTL,   KC_SPC,                  FN,      KC_LSFT, KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -78,7 +95,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, FR_A,    FR_O,    FR_E,    FR_U,    FR_I,                               FR_D,    FR_H,    FR_T,    FR_N,    FR_S,    KC_ENT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, D_COLUMN,  FR_Q,    FR_J,    FR_K,    FR_X,    KC_LGUI,          KC_END,  FR_B,    FR_M,    FR_W,    FR_V,    FR_Z,    ISRT,
+     KC_LCTL, D_COLUMN,FR_Q,    FR_J,    FR_K,    FR_X,    KC_LGUI,          KC_END,  FR_B,    FR_M,    FR_W,    FR_V,    FR_Z,    _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    KC_LALT, KC_LCTL,   KC_SPC,                  FN,      KC_LSFT, KC_RALT
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+  [_ENGRAM] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_ESC,  E_ONE,   E_TWO,   E_THREE, E_FOUR,  E_FIVE,                             E_SIX,   E_SEVEN, E_EIGHT, E_NINE,  E_ZERO,  KC_BSPC,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB,  FR_B,    FR_Y,    FR_O,    FR_U,    E_QUOT,                             E_DQUO,  FR_L,    FR_D,    FR_W,    FR_V,    FR_Z,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LSFT, FR_C,    FR_I,    FR_E,    FR_A,    E_COMM,                             E_DOT,   FR_H,    FR_T,    FR_S,    FR_N,    KC_ENT,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LCTL, FR_G,    FR_X,    FR_J,    FR_K,    E_MINS,  KC_LGUI,          KC_END,  E_QUES,  FR_R,    FR_M,    FR_F,    FR_P,    FR_Q,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LALT, KC_LCTL,   KC_SPC,                  FN,      KC_LSFT, KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -100,27 +131,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+     _______, ISRT,    DVORAK,  ENGRAM,  _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     QK_BOOT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+     _______, _______, _______, FR_GRV,  _______, FR_LBRC,                            FR_RBRC, _______, FR_DEG,  _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_DEL,  _______, KC_LEFT, KC_RGHT, KC_UP,   KC_LBRC,                            KC_RBRC, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_PIPE,
+     _______, _______, _______, FR_EURO, FR_AT,   FR_LCBR,                            FR_RCBR, FR_HASH, FR_DLR,  _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     BL_STEP, _______, _______, _______, KC_DOWN, KC_LCBR, KC_LPRN,          KC_RPRN, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS, _______,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, KC_DEL,                    KC_DEL,  _______, KC_P0
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
-  ),
-
-  [_RAISE] = LAYOUT(
-  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_MOD, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_HOME, RGB_HUI, RGB_SAI, RGB_VAI, KC_BSLS,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, KC_LPRN,          _______, KC_PLUS, KC_END,  RGB_HUD, RGB_SAD, RGB_VAD, _______,
+     _______, _______, _______, _______, _______, FR_BSLS, _______,          _______, FR_SLSH, _______, _______, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -142,6 +159,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case ENGRAM:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_ENGRAM);
+      }
+      return false;
+      break;
     case FN:
       if (record->event.pressed) {
         layer_on(_FN);
@@ -153,28 +176,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
       }
       return false;
       break;
@@ -208,6 +211,58 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case ZERO:
       SHIFT_NO(FR_RPRN, FR_0);
       break;
+
+    // ENGRAM NUMBERS
+    case E_ONE:
+      SHIFT_NO(FR_PIPE, FR_1);
+      break;
+    case E_TWO:
+      SHIFT_NO(FR_EQL, FR_2);
+      break;
+    case E_THREE:
+      SHIFT_NO(FR_TILD, FR_3);
+      break;
+    case E_FOUR:
+      SHIFT_NO(FR_PLUS, FR_4);
+      break;
+    case E_FIVE:
+      SHIFT_NO(FR_LABK, FR_5);
+      break;
+    case E_SIX:
+      SHIFT_NO(FR_RABK, FR_6);
+      break;
+    case E_SEVEN:
+      SHIFT_NO(FR_CIRC, FR_7);
+      break;
+    case E_EIGHT:
+      SHIFT_NO(FR_AMPR, FR_8);
+      break;
+    case E_NINE:
+      SHIFT_NO(FR_PERC, FR_9);
+      break;
+    case E_ZERO:
+      SHIFT_NO(FR_ASTR, FR_0);
+      break;
+
+    case E_QUOT:
+      SHIFT_NO(FR_QUOT, FR_LPRN);
+      break;
+    case E_DQUO:
+      SHIFT_NO(FR_DQUO, FR_RPRN);
+      break;
+    case E_COMM:
+      SHIFT_NO(FR_COMM, FR_SCLN);
+      break;
+    case E_DOT:
+      SHIFT_NO(FR_DOT, FR_COLN);
+      break;
+    case E_MINS:
+      SHIFT_NO(FR_MINS, FR_UNDS);
+      break;
+    case E_QUES:
+      SHIFT_NO(FR_QUES, FR_EXLM);
+      break;
+
     case QUOTES:
       SHIFT_NO(FR_QUOT, FR_DQUO);
       break;
